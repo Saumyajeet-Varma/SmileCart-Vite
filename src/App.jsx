6a53d15e30ba5039e74eb9e292f145efdd397daa
Saumyajeet-Varma <saumyajeetv@gmail.com>
2025-02-13 19:45:37 +0530

    Added Product Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Carousel from "./components/Carousel"
-
-import { IMAGE_URLS } from "./constants";
+import Product from "./components/Product"
 
 function App() {
   return (
     <div>
-      <Carousel title="Infinix Inbook" imageUrls={IMAGE_URLS} />
+      <Product />
     </div>
   )
 }
